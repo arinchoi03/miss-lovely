3039051ffa9c5ad421e3262f80a4c7bcad9764f0
Yejee Arin Choi <yej.arin.choi@gmail.com>
2017-11-11 11:26:30 -0500

    Add collection component

--- src/Collection.js
+++ src/Collection.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-// import './Collection.css';
+import './Collection.css';
 
 class Collection extends Component {
   render() {
     return (
       <div className="collection">
+        <div className="collection-left">
+          <h4>MISS LOVELY</h4>
+          <h4>ROCK COLLECTION</h4>
+          <h6>INCREDIBLE PIECES FOR EVERY OCCASSION, FIND</h6>
+          <h6>OUT MORE ABOUT THESE AMAZING PRODUCTS</h6>
+          <button type="button" className="btn btn-primary">VIEW COLLECTION</button>
+        </div>
+        <div className="collection-right">
+        </div>
       </div>
     );
   }
